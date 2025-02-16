0e90d738c12460618284b6429f71f1b3c16ad70f
skoanton <antonhakanssonn@gmail.com>
2025-02-16 18:30:23 +0100

    feat: add API key authentication for weather endpoint

--- index.js
+++ index.js
@@ -10,6 +10,18 @@ const app = express();
 app.use(express.json());
 
 app.get("/api/weather", async (req, res) => {
+
+    const authKey = req.headers['authorization'];
+    console.log(authKey);
+    console.log("API_KEY",process.env.API_KEY);
+    if(!authKey){
+        return res.status(401).json({error: "Unauthorized"});
+    }
+
+    if (authKey !== process.env.API_KEY) {
+        return res.status(403).json({ error: "Unauthorized" });
+    }
+
     try {
         const weather = await fetchWeather();
         res.json(weather);
